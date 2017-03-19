c4492fdacf3c0db2fa80d94675bd4b94b8563ec0
Hinaar <laszlo.dani@gmail.com>
2017-03-19 23:50:24 +0100

    m

--- src/application/GlobalLogger.java
+++ src/application/GlobalLogger.java
@@ -4,11 +4,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 
-
+/**
+ * Loggolo osztaly, amin duplikalunk minden kimeno es bemeno parancsot, ill. valaszt
+ * @author Tsurhe
+ *
+ */
 public class GlobalLogger {
 	static FileWriter fw;
+	
+	/**
+	 * Ezt a fuggvenyt hivva tudunk fajlba irni ( a fajl elozo tartalma nem torlodik)
+	 * @param s Az irni kivant tartalom 
+	 */
 	public static void log(String s){
 		try{
+			//a loghoz idot is adunk a konnyebb attekinthetoseg erdekeben
 			LocalDateTime ldt = LocalDateTime.now();
 			String tmp = ldt + " " + s;
 			fw.write(System.lineSeparator() + tmp );
@@ -18,20 +28,24 @@ public class GlobalLogger {
 		}
 	}
 	
+	/**
+	 * Megnyitja a fajlt a kiirashoz
+	 */
 	public static void Init(){
 			try {
 				fw = new FileWriter("Log.txt", true);
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 	}
 	
+	/**
+	 * Bezarja a fajlt
+	 */
 	public static void stop(){
 		try {
 			fw.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
