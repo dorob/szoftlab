e8ab54c96672647160c1fee1a6ec5eab9bb09141
krisztián szalkai <krisztin szalkai>
2017-04-13 12:11:08 +0200

    Scoreboard final

--- src/application/Scoreboard.java
+++ src/application/Scoreboard.java
@@ -19,13 +19,7 @@ public class Scoreboard implements Serializable{
 	public Scoreboard(){
 	//	System.out.println("called: scoreboard constructor");
 		GlobalLogger.log("called: scoreboard constructor");
-		helyezes = new ArrayList<Player>();
-		helyezes.add(new Player("a", 8));
-		helyezes.add(new Player("a", 5));
-		helyezes.add(new Player("a", 6));
-		helyezes.add(new Player("a", 7));
-		Collections.sort(helyezes);
-		
+		helyezes = new ArrayList<Player>();		
 	}
 	
 	/**
